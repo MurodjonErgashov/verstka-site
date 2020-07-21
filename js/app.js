690478bbe7894371b7b51d0fb75b417d4d5c7d45
murodjon <luckyfreelancer997357847@gmail.com>
2020-07-21 12:59:21 +0500

    Simple Single Page Web-Site-1

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scroling
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
